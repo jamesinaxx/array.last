0309d484ae2bdf5017acaa7c5bb93911e3ed1455
Juliette Cordor <professional@maybejules.com>
2024-10-18 08:43:15 +1100

    updates

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 declare global {
   interface Array<T> {
+    /**
+     * Returns the last element of an array.
+     */
     last: T;
   }
 }
 
+/**
+ * Get the last element of an array.
+ * @param {Array} array The array to get the last element from.
+ * @returns The last element of the array.
+ */
 export default function last<T>(array: Array<T>): T {
   return array.last;
 }
@@ -11,5 +19,9 @@ export default function last<T>(array: Array<T>): T {
 Object.defineProperty(Array.prototype, "last", {
   get() {
     return this[this.length - 1];
+  },
+  // deno-lint-ignore no-explicit-any
+  set(element: any) {
+    this[this.length - 1] = element;
   },
 });
--- main_test.ts
+++ main_test.ts
@@ -10,3 +10,10 @@ Deno.test(function importedLastFunction() {
   const array = [3, 2, 1];
   assertEquals(last(array), 1);
 });
+
+Deno.test(function setLast() {
+  const array = [1, 2, 3];
+  array.last = 4;
+  assertEquals(array, [1, 2, 4]);
+  assertEquals(array.last, 4);
+});
