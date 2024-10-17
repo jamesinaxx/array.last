e8ac9e82d9c282b729bd432d155160da194aa9a9
Juliette Cordor <professional@maybejules.com>
2024-10-18 09:02:00 +1100

    move global tests to seperate file

--- mod_test.ts
+++ mod_test.ts
@@ -1,19 +1,7 @@
 import { assertEquals } from "@std/assert";
 import last from "./mod.ts";
 
-Deno.test(function arrayLastMember() {
-  const array = [1, 2, 3];
-  assertEquals(array.last, 3);
-});
-
 Deno.test(function importedLastFunction() {
   const array = [3, 2, 1];
   assertEquals(last(array), 1);
-});
-
-Deno.test(function setLast() {
-  const array = [1, 2, 3];
-  array.last = 4;
-  assertEquals(array, [1, 2, 4]);
-  assertEquals(array.last, 4);
 });
