7f686988fbebbef14889367b84b6b74ca3de12b5
Juliette Cordor <professional@maybejules.com>
2024-10-18 08:47:30 +1100

    added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Array.last
+
+Get the last element of an array.
+
+## Usage
+
+```ts
+import last from "jsr:@cordor/array-last@1";
+
+const array = [1, 2, 3];
+console.log(last(array)); // 3
+```
+
+## License
+
+MIT
+
+**Made with 💗 by Juliette Cordor**
--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ declare global {
 
 /**
  * Get the last element of an array.
- * @param {Array} array The array to get the last element from.
+ * @param array The array to get the last element from.
  * @returns The last element of the array.
  */
 export default function last<T>(array: Array<T>): T {
